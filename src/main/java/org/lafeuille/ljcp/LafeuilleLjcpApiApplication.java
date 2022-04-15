443a47ab7c5a50e7d4e8939dd026452bb6b844dd
Olivier Le Merdy <olivier.lemerdy+git@gmail.com>
2022-04-15 17:39:02 +0200

    Initial commit

---
+++ src/main/java/org/lafeuille/ljcp/LafeuilleLjcpApiApplication.java
@@ -0,0 +1,13 @@
+package org.lafeuille.ljcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafeuilleLjcpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LafeuilleLjcpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lafeuille/ljcp/LafeuilleLjcpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lafeuille.ljcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafeuilleLjcpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
